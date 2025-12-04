e8e3987c2512c4a7e232f54087cdff26f0bbbd36
Erik Holmstedt <er7010ho-s@student.lu.se>
2025-12-04 10:27:38 +0100

    fuck

--- Search.java
+++ Search.java
@@ -7,17 +7,21 @@ public class Search {
 
     public List<String> searchMatchedLines(String pattern, String file) throws FileNotFoundException{
         
-        List<String> linesWithPattern = new java.util.ArrayList<>();
+        List<String> result = new java.util.ArrayList<>();
 
         Scanner scan = new Scanner(new File(file));
+
+        //Used to only add lines with the word and not lines where the word is a part of another word 
         Pattern p = Pattern.compile("\\b" + Pattern.quote(pattern) + "\\b");
+
         while (scan.hasNext()){
             String line = scan.nextLine();
             if (p.matcher(line).find()) {
-                linesWithPattern.add(line);
+                result.add(line);
             }
         }
-        return linesWithPattern;
+
+        return result;
     }
     public static void main(String[] args){
         String pattern = args[0];
